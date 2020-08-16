86b9eb2e92dab256af50377cfe23e5796ceb3524
miaonster <miaonster17@gmail.com>
2020-08-16 21:27:31 +0800

    Initial commit

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.css'
+
+export default class Index extends Component {
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+        <View>
+          <View>
+            <View>
+              <View>
+                <View>
+                  <View>
+                    <View>
+                      <View>
+                        <View>
+                          <View>
+                            <View>
+                              <View>
+                                <View>
+                                  <View>
+                                    <View>
+                                      haha
+                                    </View>
+                                  </View>
+                                </View>
+                              </View>
+                            </View>
+                          </View>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
